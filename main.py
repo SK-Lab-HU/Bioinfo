5cdcc5b4de2225d7c16cd2e27165ddf63dabf930
ikeda042 <ikeda042main@gmail.com>
2024-04-17 12:12:12 +0900

    T

--- main.py
+++ main.py
@@ -16,8 +16,18 @@ def generate_sequence_ver2(length: int) -> str:
 print(generate_sequence_ver2(30))
 
 DNA = "CGGAAGCGTGGCTTTTATTCCTCGTGACAC"
+
 print(DNA.count("A"))
 print(DNA.count("C"))   
 print(DNA.count("G"))
 print(DNA.count("T"))
 
+DNA = "CGGAAGCGTGGCTTTTATTCCTCGTGACAC"
+
+gc_rate = (DNA.count("G") + DNA.count("C")) / len(DNA)
+print(gc_rate)
+
+
+DNA = "CGGAAGCGTGGCTTTTATTCCTCGTGACAC"
+
+mRNA = DNA.replace("T", "U")
