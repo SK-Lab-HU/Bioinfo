86838636ef604ca611ba2de4b14e766b0d17d5f5
ikeda042 <ikeda042main@gmail.com>
2024-04-17 14:31:15 +0900

    T

--- search_motif.py
+++ search_motif.py
@@ -22,10 +22,31 @@ for i in sequences:
 
 print(shortest_seq_length)
 
-def LCS(s_1:str,s_2:str) -> str:
+def find_common_substr(s_1:str,s_2:str) -> str:
     for i in range(len(s_2),0,-1):
         for j in range(len(s_2)-i+1):
             substr = s_2[j:j+i]
             if substr in s_1:
                 return(substr)
     return ""
+
+
+import collections
+substring_list = [] 
+#最長モチーフの候補を格納するリストを作成
+for i in range(len(sequences)-1):
+    s_1 = sequences[i]
+    s_2 = sequences[i+1]
+    common_sub_str = find_common_substr(s_1,s_2)
+    print(common_sub_str)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    substring_list.append(common_sub_str)
+
+#collectionsのCounterクラスを使用してsubstring_list中の頻度順に辞書d_substrに登録
+d_substr = collections.Counter(substring_list)
+lcs = next(iter(d_substr))
+print(d_substr)
+for n,i in enumerate(sequences):
+    if lcs in i:
+        print(n)
+        
