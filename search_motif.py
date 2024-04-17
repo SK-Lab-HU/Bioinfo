7f53eb72712bbaa54f5223c0cd8bc1cfc1a36f09
ikeda042 <ikeda042main@gmail.com>
2024-04-17 14:38:53 +0900

    featL motif

--- search_motif.py
+++ search_motif.py
@@ -1,3 +1,5 @@
+import collections
+
 with open("data.txt") as f:
     lines = [i.replace("\n","") for i in f.readlines()]
 
@@ -31,21 +33,24 @@ def find_common_substr(s_1:str,s_2:str) -> str:
     return ""
 
 
-import collections
+
 substring_list = [] 
 #最長モチーフの候補を格納するリストを作成
-for i in range(len(sequences)-1):
-    s_1 = sequences[i]
-    s_2 = sequences[i+1]
-    common_sub_str = find_common_substr(s_1,s_2)
-    print(common_sub_str)
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    substring_list.append(common_sub_str)
+for i in range(len(sequences)):
+    for j in range(len(sequences)):
+        if i != j:
+            common_sub_str = find_common_substr(sequences[i],sequences[j])
+            print(i)
+            print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            substring_list.append(common_sub_str)
 
 #collectionsのCounterクラスを使用してsubstring_list中の頻度順に辞書d_substrに登録
 d_substr = collections.Counter(substring_list)
 lcs = next(iter(d_substr))
 print(d_substr)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print(lcs)
+# すべての配列の部分列であるかを検証
 for n,i in enumerate(sequences):
     if lcs in i:
         print(n)
