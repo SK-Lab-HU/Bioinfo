fef55e8c259fe7c831582780552161a96f4b98f5
ikeda042 <ikeda042main@gmail.com>
2024-04-17 11:59:21 +0900

    T

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def generate_sequence(length: int) -> str:
     return "".join([random.choice(nucleotides) for _ in range(length)])
 
 print(generate_sequence(10))
+
+def generate_sequence_ver2(length: int) -> str:
+    DNA = ""
+    for _ in range(length):
+        DNA += random.choice(nucleotides)
+    return DNA
+    
