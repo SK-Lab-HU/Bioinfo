9237cf791da9b9c4ae556c9c4f736d20f6f53833
ikeda042 <ikeda042main@gmail.com>
2024-04-17 12:00:34 +0900

    T

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import random
 
 nucleotides = ["A", "C", "G", "T"]
 
-def generate_sequence(length: int) -> str:
-    return "".join([random.choice(nucleotides) for _ in range(length)])
-
-print(generate_sequence(10))
-
-def generate_sequence_ver2(length: int) -> str:
+def generate_sequence_ver1(length: int) -> str:
     DNA = ""
     for _ in range(length):
         DNA += random.choice(nucleotides)
     return DNA
+
+print(generate_sequence_ver1(10))
     
+def generate_sequence_ver2(length: int) -> str:
+    return "".join([random.choice(nucleotides) for _ in range(length)])
+
+print(generate_sequence_ver2(10))
+
+
+
